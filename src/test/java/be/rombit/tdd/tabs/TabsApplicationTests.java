c39836f06bfc363cb3bd33292f1ede391a401ac8
Vince Verberckt <vince@verberckt.com>
2020-01-25 10:55:26 +0100

    Initial commit

---
+++ src/test/java/be/rombit/tdd/tabs/TabsApplicationTests.java
@@ -0,0 +1,13 @@
+package be.rombit.tdd.tabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
