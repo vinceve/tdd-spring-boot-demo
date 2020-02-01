d06e51f57982b29f6425644ee680f6fef482192c
Vince Verberckt <vince@verberckt.com>
2020-02-01 14:50:42 +0100

    Implement the create action

--- src/main/java/be/rombit/tdd/tabs/controllers/ArtistController.java
+++ src/main/java/be/rombit/tdd/tabs/controllers/ArtistController.java
@@ -3,10 +3,9 @@ package be.rombit.tdd.tabs.controllers;
 import be.rombit.tdd.tabs.models.entities.Artist;
 import be.rombit.tdd.tabs.services.ArtistService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,4 +21,9 @@ public class ArtistController {
         return ResponseEntity.ok(artists);
     }
 
+    @PostMapping
+    public ResponseEntity<String> create(@RequestBody Artist artist) {
+        this.artistService.create(artist);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
 }
--- src/test/java/be/rombit/tdd/tabs/integrations/ArtistResourceTests.java
+++ src/test/java/be/rombit/tdd/tabs/integrations/ArtistResourceTests.java
@@ -3,6 +3,7 @@ package be.rombit.tdd.tabs.integrations;
 import be.rombit.tdd.tabs.DataFactory;
 import be.rombit.tdd.tabs.models.entities.Artist;
 import be.rombit.tdd.tabs.models.repositories.ArtistRepository;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +14,6 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import javax.transaction.Transactional;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -27,8 +27,12 @@ public class ArtistResourceTests {
     @Autowired
     private ArtistRepository artistRepository;
 
+    @AfterEach
+    public void cleanupDatabase() {
+        this.artistRepository.deleteAll();
+    }
+
     @Test
-    @Transactional
     public void listReturnsA200Status() {
         ParameterizedTypeReference<List<Artist>> artistTypeDefinition = new ParameterizedTypeReference<List<Artist>>() {};
         ResponseEntity<List<Artist>> response = this.restTemplate.exchange("/artists", HttpMethod.GET, null, artistTypeDefinition);
@@ -37,7 +41,6 @@ public class ArtistResourceTests {
     }
 
     @Test
-    @Transactional
     public void listReturnsArtists() {
         Artist artist = new DataFactory().getArtist();
         this.artistRepository.save(artist);
@@ -46,5 +49,33 @@ public class ArtistResourceTests {
         ResponseEntity<List<Artist>> response = this.restTemplate.exchange("/artists", HttpMethod.GET, null, artistTypeDefinition);
 
         assertEquals(1, response.getBody().size());
+    }
+
+    @Test
+    public void createReturnsA201Status() {
+        Artist artist = new DataFactory().getArtist();
+        ResponseEntity<Artist> response = this.restTemplate.postForEntity("/artists", artist, Artist.class);
+
+        assertEquals(201, response.getStatusCode().value());
+    }
+
+    @Test
+    public void createSavesTheArtist() {
+        Artist artist = new DataFactory().getArtist();
+        this.restTemplate.postForEntity("/artists", artist, Artist.class);
+
+        Artist savedArtist = this.artistRepository.findAll().get(0);
+
+        assertEquals(artist.getName(), savedArtist.getName());
+    }
+
+    @Test
+    public void createReturnsA422StatusOnValidationErrors() {
+        Artist artist = new DataFactory().getArtist();
+        artist.setName(null);
+
+        ResponseEntity<Artist> response = this.restTemplate.postForEntity("/artists", artist, Artist.class);
+
+        assertEquals(422, response.getStatusCode().value());
     }
 }
